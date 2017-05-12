c804cfd5a6e6f67756a079ebbe268ac78c0b9a3b
Raphael Amiard <amiard@adacore.com>
2017-05-12 13:29:13 +0200

    [nvim_gdb]: Nvim plugin

---
+++ src/rplugin/python/nvim_gdb.py
@@ -0,0 +1,15 @@
+import neovim
+
+
+@neovim.plugin
+class GdbPlugin(object):
+
+    def __init__(self, vim):
+        self.vim = vim
+
+    @neovim.command('StartGdb', range='', nargs='*', sync=True)
+    def command_handler(self, args, range):
+        gdb_args = [
+            "gdb", "-ex", "\"python import nvgdb_module\""
+        ] + args
+        self.vim.call("termopen", " ".join(gdb_args))
